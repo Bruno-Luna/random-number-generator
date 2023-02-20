d19a4ef374fa826367608cb8e16d0ab507f773ab
Bruno-Luna <luna.brsilva@gmail.com>
2023-02-20 07:30:35 -0300

    feat: funções, variáveis e métodos

---
+++ assets/scripts/app.js
@@ -0,0 +1,33 @@
+const inputOf = document.querySelector('.numberOf')
+const inputUpTo = document.querySelector('.numberUpTo')
+const btnGenerate= document.querySelector('.generate')
+const btnClear= document.querySelector('.clear')
+const paragraph = document.querySelector('p')
+
+
+btnGenerate.addEventListener('click', () => {
+    if(convertStringToNumber()){
+        const randomNumber = Math.random() 
+        const numRandom = Math.round(randomNumber * (inputUpTo.value - inputOf.value) + inputOf.value)
+        
+        paragraph.textContent = numRandom
+    }
+})
+
+btnClear.addEventListener('click', () => {
+    inputOf.value = ''
+    inputUpTo.value = ''
+    paragraph.textContent = ''
+})
+
+const convertStringToNumber = () => {
+    const numUpToConverte = Number(inputUpTo.value) 
+    const numOfConverte = Number(inputOf.value)
+
+    if(numUpToConverte < numOfConverte){
+        alert('Número não pode ser menor que o inicial!')
+        return false
+    }
+
+    return true
+}
