878be214db82c40194b3e6b651314cf1a1081444
Bruno-Luna <luna.brsilva@gmail.com>
2023-02-22 15:07:54 -0300

    refactor: desaclopamento das funções e organização das variáveis

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,28 +1,13 @@
-const inputOf = document.querySelector('.numberOf')
-const inputUpTo = document.querySelector('.numberUpTo')
 const btnGenerate= document.querySelector('.generate')
-const btnClear = document.querySelector('.clear')
+const inputUpTo = document.querySelector('.numberUpTo')
 const paragraph = document.querySelector('p')
-
-btnGenerate.addEventListener('click', () => {
-    if(convertStringToNumber()){
-        const randomNumber = Math.random() 
-        const numRandom = Math.round(randomNumber * (inputUpTo.value - inputOf.value) + inputOf.value)
-
-        paragraph.textContent =`Número sorteado: ${numRandom}`
-    }
-})
-
-btnClear.addEventListener('click', () => {
-    inputOf.value = ''
-    inputUpTo.value = ''
-    paragraph.textContent = ''
-})
+const btnClear = document.querySelector('.clear')
+const inputOf = document.querySelector('.numberOf')
 
 const convertStringToNumber = () => {
     const numUpToConverte = Number(inputUpTo.value) 
     const numOfConverte = Number(inputOf.value)
-
+    
     if(numUpToConverte < numOfConverte){
         alert('Número não pode ser menor que o inicial!')
         return false
@@ -35,3 +20,21 @@ const convertStringToNumber = () => {
 
     return true
 }
+
+const generateNumber = () => {
+    if(convertStringToNumber()){
+        const randomNumber = Math.random() 
+        const numRandom = Math.round(randomNumber * (inputUpTo.value - inputOf.value) + inputOf.value)
+        
+        paragraph.textContent =`Número sorteado: ${numRandom}`
+    }
+}
+
+const clearValues = () => {
+    inputOf.value = ''
+    inputUpTo.value = ''
+    paragraph.textContent = ''
+}
+
+btnClear.addEventListener('click', clearValues)
+btnGenerate.addEventListener('click', generateNumber)
